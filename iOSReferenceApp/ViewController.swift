0dc84f29a2db4ae3cb27f5f46323ee0f1fa9dbaf
Lucas Senn <lucas.senn@moost.io>
2022-12-19 13:16:09 +0100

    Initial Commit

---
+++ iOSReferenceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSReferenceApp
+//
+//  Created by Lucas Senn on 19.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
